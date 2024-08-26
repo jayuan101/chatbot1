e9a329b009c04a764140a8ba45f8483d7cd6e509
jayuan101 <43624484+jayuan101@users.noreply.github.com>
2024-08-26 12:27:04 -0400

    Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -7,51 +7,19 @@ Original file is located at
     https://colab.research.google.com/drive/12M459BWVXzkmIPR7Ttlj5QA73IQLaO10
 """
 
-import os
-import streamlit as st
-from pandasai import PandasAI
-from pandasai.llm.openai import OpenAI
-from data import load_data
-from pandasai.responses.response_parser import ResponseParser
+import pickle
+from pathlib import Path
+import pandas as pd
+import streamlit
 
-class StreamlitResponse(ResponseParser):
-  def __init__(self, context) -> None:
-    super().__init__(context)
+def load_file(path: str) -> str:
+    with open(path, "rb") as f:
+      dataset = pickle.load(f)
+    return dataset
 
-  def format_dataframe(self, result):
-    st.Dataframe(result["value"])
-    return result
-
-  def format_plot(self, result):
-    st.image(result["value"])
-    return result
-  def format_other(self, result):
-    st.write(result["value"])
-
-class StreamlitCallback(BaseCallback):
-  def __init__ (self, container) -> None:
-    super().__init__(container)
-  def on_code(self,response: str):
-    self.container.code(response)
-
-st.write('Chat Credit Cardd Fraud')
-
-df = data.load_data("./data")
-
-with st.expander ("see datasets preview"):
-st.write(df.tail(100))
-
-query = st.text_input("Chat with Dataframe")
-container = st.container()
-
-if query:
-  llm = OpenAI(api_token=os.environ.get("OPENAI_API_KEY"))
-  query_engine = StartDataframe(
-      df,
-      config={
-        "llm": llm
-      "reponce_parser": StreamlitResponse,
-      "Callback":StreamlitCallback(container),
-      },
-  )
-  answer = query_engine.query(query)
+# Initialize the page
+@st.cache_data
+def load_data(folder: str)-> pd.DataFrame:
+  all_dataset = [load_file(file) for file in Path(folder).interdir()]
+  df = pd.concat (all_datasets)
+  return df
